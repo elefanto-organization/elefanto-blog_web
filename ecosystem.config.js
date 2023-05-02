ead112a79c554c78c959f47572a81264c14b6531
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-05-02 16:32:24 +0600

    fix(ecosystem): file ffrom mjs to nuxt js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'Elefanto Blog',
       exec_mode: 'cluster',
       instances: '2', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.mjs',
+      script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
   ]
